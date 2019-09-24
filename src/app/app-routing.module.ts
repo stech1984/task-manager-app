d147454654aa291910cb855ff85be39fe8cb3289
Sumit <sumitgupta28@gmail.com>
2019-09-23 20:31:53 -0500

    init commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+import { TaskDetailsComponent } from './task-details/task-details.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { UpdateTaskComponent } from './update-task/update-task.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'addTask', component: CreateTaskComponent },
+  { path: 'details/:taskId', component: TaskDetailsComponent },
+  { path: 'updateTask/:taskId', component: UpdateTaskComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
